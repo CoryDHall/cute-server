e20818b8597681022184cae831d74187dd854827
Cory Hall <corydantehall@gmail.com>
2016-02-02 16:50:28 -0500

    Add verbosity to Server responseHandler.

--- lserver.es6
+++ lserver.es6
@@ -6,26 +6,33 @@ const http = require('http')
     , mime = require('mime')
     , mnm  = require('minimist');
 
-let {
+const {
     p: port=8000,
     r: root=process.cwd(),
-    f: fallbackPath
+    f: relativeFallback
     } = mnm(process.argv.slice(2));
 
-if (fallbackPath) fallbackPath = path.join(root, fallbackPath);
+let fallbackPath;
+
+if (relativeFallback) fallbackPath = path.join(root, relativeFallback);
 
 http.createServer(function requestHandler(req, res) {
   let uriPath  = url.parse(req.url).pathname,
-      filePath = path.join(root, unescape(uriPath));
+      mappedFilePath = path.join(root, unescape(uriPath));
 
-  console.log('Serving ' + uriPath);
-  handle(filePath);
+  console.log(`\n\n  Started Serving "\x1b[1m${uriPath}\x1b[22m" at ${new Date()}:`);
+  console.time(`Served "\x1b[37m\x1b[1m${uriPath}\x1b[0m" in`);
+  handle(mappedFilePath);
 
   function handle(filePath, fallingback) {
     fs.stat(filePath, function(err, stat) {
       if (err) {
+        console.log(`\t\x1b[31mUnable to load \x1b[0m"\x1b[1m${mappedFilePath}\x1b[22m"`);
         if (err.code == 'ENOENT') {
-          if (!fallingback && fallbackPath) return handle(fallbackPath, true);
+          if (!fallingback && fallbackPath) {
+            console.log(`\t\x1b[35mAttemping to serve \x1b[0m"\x1b[1m${relativeFallback}\x1b[22m"`);
+            return handle(fallbackPath, true);
+          }
           res.statusCode = 404;
         }
         else res.statusCode = 500;
@@ -38,12 +45,17 @@ http.createServer(function requestHandler(req, res) {
       }
 
       else {
+        console.time(`Loaded "\x1b[37m\x1b[1m${filePath}\x1b[0m" in`);
         let contentType = mime.lookup(path.extname(filePath));
         res.writeHead(200, { 'Content-Type': contentType });
         fs.createReadStream(filePath).pipe(res);
+        console.log(`\t\x1b[36mResponding with \x1b[0m"\x1b[1m${filePath}\x1b[22m"${strDivide()}\n`);
+        console.timeEnd(`Loaded "\x1b[37m\x1b[1m${filePath}\x1b[0m" in`);
+        console.timeEnd(`Served "\x1b[37m\x1b[1m${uriPath}\x1b[0m" in`);
       }
     });
   }
+  function strDivide() { return `\n${new Array(60).fill("=").join('')}` }
 }).listen(port);
 
-console.log('Server running at http://localhost:' + port + '/' + ' [root: ' + root + ', fallback: ' + fallbackPath + ']')
+console.log(`\x1b[1mServer running at http://localhost:${port}/ [root: ${root}, fallback: ${fallbackPath}]\x1b[22m`)
--- lserver.js
+++ lserver.js
@@ -14,22 +14,29 @@ var _mnm$p = _mnm.p;
 var port = _mnm$p === undefined ? 8000 : _mnm$p;
 var _mnm$r = _mnm.r;
 var root = _mnm$r === undefined ? process.cwd() : _mnm$r;
-var fallbackPath = _mnm.f;
+var relativeFallback = _mnm.f;
 
-if (fallbackPath) fallbackPath = path.join(root, fallbackPath);
+var fallbackPath = undefined;
+
+if (relativeFallback) fallbackPath = path.join(root, relativeFallback);
 
 http.createServer(function requestHandler(req, res) {
   var uriPath = url.parse(req.url).pathname,
-      filePath = path.join(root, unescape(uriPath));
+      mappedFilePath = path.join(root, unescape(uriPath));
 
-  console.log('Serving ' + uriPath);
-  handle(filePath);
+  console.log('\n\n  Started Serving "\u001b[1m' + uriPath + '\u001b[22m" at ' + new Date() + ':');
+  console.time('Served "\u001b[37m\u001b[1m' + uriPath + '\u001b[0m" in');
+  handle(mappedFilePath);
 
   function handle(filePath, fallingback) {
     fs.stat(filePath, function (err, stat) {
       if (err) {
+        console.log('\t\u001b[31mUnable to load \u001b[0m"\u001b[1m' + mappedFilePath + '\u001b[22m"');
         if (err.code == 'ENOENT') {
-          if (!fallingback && fallbackPath) return handle(fallbackPath, true);
+          if (!fallingback && fallbackPath) {
+            console.log('\t\u001b[35mAttemping to serve \u001b[0m"\u001b[1m' + relativeFallback + '\u001b[22m"');
+            return handle(fallbackPath, true);
+          }
           res.statusCode = 404;
         } else res.statusCode = 500;
         res.end();
@@ -37,12 +44,19 @@ http.createServer(function requestHandler(req, res) {
       } else if (stat.isDirectory()) {
         handle(path.join(filePath, 'index.html'));
       } else {
+        console.time('Loaded "\u001b[37m\u001b[1m' + filePath + '\u001b[0m" in');
         var contentType = mime.lookup(path.extname(filePath));
         res.writeHead(200, { 'Content-Type': contentType });
         fs.createReadStream(filePath).pipe(res);
+        console.log('\t\u001b[36mResponding with \u001b[0m"\u001b[1m' + filePath + '\u001b[22m"' + strDivide() + '\n');
+        console.timeEnd('Loaded "\u001b[37m\u001b[1m' + filePath + '\u001b[0m" in');
+        console.timeEnd('Served "\u001b[37m\u001b[1m' + uriPath + '\u001b[0m" in');
       }
     });
   }
+  function strDivide() {
+    return '\n' + new Array(60).fill("=").join('');
+  }
 }).listen(port);
 
-console.log('Server running at http://localhost:' + port + '/' + ' [root: ' + root + ', fallback: ' + fallbackPath + ']');
+console.log('\u001b[1mServer running at http://localhost:' + port + '/ [root: ' + root + ', fallback: ' + fallbackPath + ']\u001b[22m');
